1b3a51eaa464539c1fba525fe84ead1d76ff21a8
Steven Smith <stevesmithci7@gmail.com>
2018-05-24 14:27:06 -0700

    initial

---
+++ server.js
@@ -0,0 +1,36 @@
+//=============================================================
+// Dependencies
+// ============================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+//=============================================================
+// Sets up the Express App
+//=============================================================
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+
+//=============================================================
+//Routing
+//=============================================================
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+
+// =============================================================
+// Starts the server to begin listening 
+// =============================================================
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log('App listening on PORT ' + process.env.PORT);
+});
